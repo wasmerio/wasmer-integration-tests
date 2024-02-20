12da7d81defff623f64f4a988ca6b95979694081
Christoph Herzog <chris@theduke.at>
2024-02-20 22:01:07 +0100

    Add static site test

--- watest/src/tests/app_flow.rs
+++ watest/src/tests/app_flow.rs
@@ -111,3 +111,112 @@ async fn test_app_creation_and_deletion_through_cli() {
     }
 }
 
+/// Create a new static site app, update it and ensure the updated app is deployed.
+#[test_log::test(tokio::test)]
+async fn test_cli_app_create_static_site_and_update_multiple_times() {
+    let name = format!(
+        "test-staticsite-{}",
+        uuid::Uuid::new_v4().to_string().replace("-", "")
+    );
+    let namespace = test_namespace();
+
+    // Create static site app.
+    let client = api_client();
+
+    let dir = build_clean_test_app_dir(&name);
+
+    tracing::debug!(local_path=%dir.display(), "creating app with cli");
+
+    let status = std::process::Command::new("wasmer")
+        .args(&[
+            "app",
+            "create",
+            "-t",
+            "static-website",
+            "--non-interactive",
+            "--owner",
+            &namespace,
+            "--new-package-name",
+            &name,
+            "--name",
+            &name,
+            "--path",
+            dir.to_str().unwrap(),
+        ])
+        .spawn()
+        .expect("Failed to invoke 'wasmer app create'")
+        .wait()
+        .expect("'wasmer app create' command failed");
+    if !status.success() {
+        panic!(
+            "'wasmer app create' command failed with status: {:?}",
+            status
+        );
+    }
+
+    let html_path = dir.join("public").join("index.html");
+    if !html_path.exists() {
+        panic!("index.html does not exist: {}", html_path.display());
+    }
+
+    // Query the app.
+    let app = wasmer_api::backend::get_app(&client, namespace.clone(), name.clone())
+        .await
+        .expect("could not query app")
+        .expect("queried app is None");
+    let url = app
+        .url
+        .parse::<url::Url>()
+        .expect("Failed to parse app URL");
+    tracing::debug!(?app, "app deployed, sending request");
+
+    // Publish the app 3 times and wait for it to update.
+    for index in 1..3 {
+        let start = std::time::Instant::now();
+        tracing::debug!("updating app to version {}", index);
+        let content = format!("v{}", index);
+        std::fs::write(&html_path, &content).expect("Failed to write to index.html");
+
+        let status = std::process::Command::new("wasmer")
+            .args(&[
+                "deploy",
+                "--publish-package",
+                "--no-persist-id",
+                "--path",
+                dir.to_str().unwrap(),
+            ])
+            .spawn()
+            .expect("Failed to invoke 'wasmer deploy'")
+            .wait()
+            .expect("'wasmer deploy' command failed");
+        if !status.success() {
+            panic!("'wasmer deploy' command failed with status: {:?}", status);
+        }
+
+        loop {
+            tokio::time::sleep(std::time::Duration::from_millis(500)).await;
+
+            let res = http_client()
+                .get(url.clone())
+                .send()
+                .await
+                .expect("Failed to send request")
+                .error_for_status()
+                .expect("Failed to get response");
+
+            let body = res.text().await.expect("Failed to get response body");
+
+            if body == *content {
+                tracing::debug!(?app, "app updated to version {}", index);
+                break;
+            } else {
+                tracing::trace!(?app, "app not updated  to version {index} yet, waiting...");
+                let elaped = start.elapsed();
+                let timeout = std::time::Duration::from_secs(120);
+                if elaped > timeout {
+                    panic!("app not updated to version {index} after {timeout:?} seconds - got '{body}' instead of '{content}'");
+                }
+            }
+        }
+    }
+}
