658a8efd397f2d956674b42132cb16ffe27a1fef
Baran Karaaslan <baran@wasmer.io>
2024-02-23 09:27:01 +0300

    rename fixtures

--- tests/edge/test_restart_instance.py
+++ tests/edge/test_restart_instance.py
@@ -20,21 +20,21 @@ def wait_until_app_ready(appname):
     assert False
 
 @fixture
-def _test_app_package():
+def app_package():
     run(f"wasmer publish --registry http://localhost:8080/graphql --token wap_default_token", cwd="packages/static-web-server", shell=True)
     run(f"wasmer publish --registry http://localhost:8080/graphql --token wap_default_token", cwd="packages/test-app", shell=True)
     yield
     # TODO: Delete package when cli implememts it
 
 @fixture
-def _test_app(_test_app_package, tmpdir):
+def app(app_package, tmpdir):
     name = uuid4()
     run(f"wasmer app create --registry http://localhost:8080/graphql --token wap_default_token --non-interactive --type http --package cypress1/test-app@0.2.0 --owner cypress1 --name {name} --no-wait", shell=True, cwd=tmpdir).check_returncode()
     # assert 200 == get("http://localhost", headers={"Host": app_hostname(name)}).status_code
     yield name
     run(f"wasmer app delete --registry http://localhost:8080/graphql --token wap_default_token {name}", shell=True).check_returncode()
     
-def test_restart_instance(_test_app):
-    wait_until_app_ready(_test_app)
+def test_restart_instance(app):
+    wait_until_app_ready(app)
     sleep(35)
-    app_ping(_test_app)
+    app_ping(app)
