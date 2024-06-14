214659a53938efb000a4c891e7d47130a80fcd54
Christoph Herzog <chris@theduke.at>
2024-06-14 17:42:55 +0200

    Add instaboot PHP opcache test

--- tests/cli_actions/deploy.rs
+++ tests/cli_actions/deploy.rs
@@ -87,3 +87,123 @@ package: .
         .success());
 }
 
+/// Deploy a PHP app with instaboot, and make sure a journal-restored instance
+/// has PHP files cached in the opcache.
+#[test_log::test(tokio::test)]
+async fn test_app_instaboot_php_opcache() {
+    let dir = TempDir::new().unwrap();
+    let path = dir.path();
+    let watest::TestEnv {
+        namespace,
+        app_domain,
+        ..
+    } = watest::env();
+
+    let name = format!("test-{}", Uuid::new_v4());
+
+    let domain = format!("{name}.{app_domain}");
+
+    mkdir!(path; {
+        "wasmer.toml" => format!(r#"
+            [dependencies]
+            "php/php" = "8.3.4"
+
+            [fs]
+            "app" = "./app"
+            "config" = "./config"
+
+            [[command]]
+            name = "run"
+            module = "php/php:php"
+            runner = "wasi"
+
+            [command.annotations.wasi]
+            main-args = ["-t", "/app", "-S", "0.0.0.0:8080"]
+            env = ["PHPRC=/config/php.ini"]
+
+        "#),
+
+        "config/php.ini" => r#"
+opcache.validate_timestamps = 0
+opcache.file_update_protection = 0
+opcache.max_file_size = 0
+"#,
+        "app/index.php" => r#"
+        <?php
+
+        print(json_encode(opcache_get_status(), JSON_PRETTY_PRINT));
+        "#,
+
+        "app.yaml" => format!(r#"
+kind: wasmer.io/App.v0
+name: {name}
+owner: {namespace}
+package: .
+debug: true
+domains:
+  - {domain}
+capabilities:
+  instaboot:
+    requests:
+      - path: /
+      - path: /
+      - path: /
+"#),
+    });
+
+    assert_cmd::Command::new("wasmer")
+        .args(&["deploy", "--non-interactive", "--no-wait"])
+        .current_dir(&dir)
+        .assert()
+        .success();
+
+    let url = format!("https://{domain}/");
+
+    eprintln!("fetching url {url}");
+
+    {
+        let res = reqwest::Client::new()
+            .get(&url)
+            .header("x-edge-purge-instances", "1")
+            .send()
+            .await
+            .unwrap()
+            .error_for_status()
+            .unwrap();
+
+        assert_eq!(
+            res.headers()
+                .get("x-edge-instance-journal-status")
+                .expect("missing required header")
+                .to_str()
+                .unwrap(),
+            "none",
+        );
+
+        let data = res.json::<serde_json::Value>().await.unwrap();
+        let cache_hits = data["opcache_statistics"]["hits"].as_u64().unwrap();
+        assert_eq!(cache_hits, 0);
+    }
+
+    let res = reqwest::Client::new()
+        .get(&url)
+        .header("x-edge-purge-instances", "1")
+        .send()
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    assert_eq!(
+        res.headers()
+            .get("x-edge-instance-journal-status")
+            .expect("missing required header")
+            .to_str()
+            .unwrap(),
+        "bootsrap=journal+memory",
+    );
+
+    let data = res.json::<serde_json::Value>().await.unwrap();
+    let cache_hits = data["opcache_statistics"]["hits"].as_u64().unwrap();
+    assert!(cache_hits > 0);
+}
