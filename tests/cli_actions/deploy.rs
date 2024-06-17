fc0ea7dfd590fc189f1b93bdda242ebbc2a70df3
Christoph Herzog <chris@theduke.at>
2024-06-17 14:44:19 +0200

    Add Instaboot max age test

--- tests/cli_actions/deploy.rs
+++ tests/cli_actions/deploy.rs
@@ -1,5 +1,6 @@
 use std::fs::write;
 use std::process::Command;
+use std::time::Duration;
 use tempfile::TempDir;
 use uuid::Uuid;
 use watest::deploy_dir;
@@ -314,4 +315,153 @@ capabilities:
     let body = res.text().await.unwrap();
     eprintln!("body: '{body}'");
     assert_eq!(body, "hello", "unexpected file body");
+}
+
+/// Instaboot max_age test.
+///
+/// Ensures that the max_age config option is respected by Edge.
+///
+/// Uses a PHP app that creates a timestamp file during instaboot, and
+/// then returns that timestamp value in responses.
+///
+/// PHP app running the local php-instaboot-timestamp package.
+#[test_log::test(tokio::test)]
+async fn test_app_instaboot_max_age_php() {
+    // Ensure submodules are cloned, because the php-testserver package from
+    // the wasmopticon submodule is needed.
+    watest::ensure_submodules();
+
+    let dir = TempDir::new().unwrap();
+    let path = dir.path();
+    let watest::TestEnv {
+        namespace,
+        app_domain,
+        ..
+    } = watest::env();
+
+    let name = format!("test-{}", Uuid::new_v4());
+    let domain = format!("{name}.{app_domain}");
+
+    let pkg_dir = watest::manifest_dir()
+        .join("fixtures")
+        .join("php-instaboot-timestamp")
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    // Note: the config specifies a max_age of 10s!
+    mkdir!(path; {
+        "app.yaml" => format!(r#"
+kind: wasmer.io/App.v0
+name: {name}
+owner: {namespace}
+package: {pkg_dir}
+debug: true
+domains:
+  - {domain}
+capabilities:
+  instaboot:
+    max_age: 10s
+    requests:
+      - path: /
+"#),
+    });
+
+    eprintln!("deploying app...");
+    assert_cmd::Command::new("wasmer")
+        .args(&["deploy", "--non-interactive", "--no-wait"])
+        .current_dir(&dir)
+        .assert()
+        .success();
+
+    let url = format!("https://{domain}/");
+
+    eprintln!("fetching url {url}");
+
+    let client = http_client();
+
+    {
+        let res = client
+            .get(&url)
+            .header("x-edge-purge-instances", "1")
+            .send()
+            .await
+            .unwrap()
+            .error_for_status()
+            .unwrap();
+
+        assert_eq!(
+            res.headers()
+                .get("x-edge-instance-journal-status")
+                .expect("missing required header")
+                .to_str()
+                .unwrap(),
+            "none",
+        );
+    }
+
+    eprintln!("fetching url: {url}");
+    let res = client
+        .get(&url)
+        .header("x-edge-purge-instances", "1")
+        .send()
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    assert_eq!(
+        res.headers()
+            .get("x-edge-instance-journal-status")
+            .expect("missing required header")
+            .to_str()
+            .unwrap(),
+        "bootsrap=journal+memory",
+    );
+
+    let body = res.text().await.unwrap();
+    eprintln!("body: '{body}'");
+
+    let timestamp1 = body
+        .trim()
+        .parse::<u64>()
+        .expect("could not parse timestamp in body: not a number");
+
+    // Sleep at least 10 seconds to ensure the max_age limit is reached.
+    eprintln!("waiting for expiration...");
+    tokio::time::sleep(Duration::from_secs(13)).await;
+
+    // Edge should now be forced to create a new snapshot.
+
+    let _res = client
+        .get(&url)
+        .header("x-edge-purge-instances", "1")
+        .send()
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap();
+
+    let body2 = client
+        .get(&url)
+        .header("x-edge-purge-instances", "1")
+        .send()
+        .await
+        .unwrap()
+        .error_for_status()
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let timestamp2 = body2
+        .trim()
+        .parse::<u64>()
+        .expect("could not parse timestamp in body: not a number");
+
+    eprintln!("timestamp1: {timestamp1}, timestamp2: {timestamp2}");
+    assert!(
+        timestamp2 > timestamp1,
+        "expected timestamp to be greater (meaning the journal was purged)"
+    );
 }
