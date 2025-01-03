3266fa93b3ba411e1f20b8e09d93a1caa29fd88a
Ayush Jha <ayush@wasmer.io>
2025-01-03 16:48:54 +0545

    add flake support
    also ignore .envrc

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Wasmer Integration Tests";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+          with pkgs;
+          {
+            devShells.default = mkShell {
+              buildInputs = [ deno wasmer ];
+              shellHook = ''
+                alias test-all='deno test --allow-all --parallel'
+                echo "Run tests with: test-all"
+              '';
+            };
+          }
+      );
+}
