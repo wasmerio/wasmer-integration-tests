be888101b035412958f580ad43c13795408aabd4
Christoph Herzog <chris@theduke.at>
2025-01-28 14:04:33 +0100

    refactor: Move helper code to separate files
    The test.ts file got quite messy, being 2k+ lines long...

---
+++ src/http.ts
@@ -0,0 +1,101 @@
+import http from "node:http";
+import https from "node:https";
+import {Buffer} from 'node:buffer';
+
+// Custom nodejs based http client.
+//
+// Needed to allow custom dns resolution and accepting invalid certs.
+export class HttpClient {
+  targetServer: string | null = null;
+
+  async fetch(url: string, options: RequestInit): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      const parsedUrl = new URL(url);
+      const protocol = parsedUrl.protocol === "https:" ? https : http;
+
+      const requestHeaders: http.OutgoingHttpHeaders = {};
+      for (const [key, value] of Object.entries(options.headers ?? {})) {
+        requestHeaders[key] = value;
+      }
+
+      let lookup: any = null;
+      if (this.targetServer) {
+        const ipProto = this.targetServer.includes(":") ? 6 : 4;
+        lookup = (_hostname: string, _options: any, callback: any) => {
+          callback(null, this.targetServer, ipProto);
+          throw new Error("lookup called");
+        };
+      }
+
+      const requestOptions = {
+        method: options.method || "GET",
+        headers: requestHeaders,
+        lookup,
+      };
+
+      const req = protocol.request(parsedUrl, requestOptions, (res) => {
+        let data: any[] = [];
+
+        res.on("data", (chunk) => {
+          data.push(chunk);
+        });
+
+        res.on("end", () => {
+          const plainHeaders: Record<string, string> = {};
+          for (const [key, value] of Object.entries(res.headers)) {
+            if (value) {
+              if (typeof value === "string") {
+                plainHeaders[key] = value;
+              } else {
+                throw new Error(
+                  `could not convert header value: ${key}: ${typeof value}`,
+                );
+              }
+            }
+          }
+
+          const headers = new Headers(plainHeaders);
+
+          const buffer = Buffer.concat(data);
+          const bodyArray: Uint8Array = new Uint8Array(buffer);
+
+          const status = res.statusCode || 0;
+          const out: Response = {
+            ok: status >= 200 && status < 300,
+            status,
+            statusText: res.statusMessage ?? "unknown",
+            json: () => Promise.resolve(JSON.parse(buffer.toString())),
+            text: () => Promise.resolve(buffer.toString()),
+            bytes: () => Promise.resolve(bodyArray),
+            arrayBuffer: () => Promise.resolve(buffer),
+            headers,
+            url: res.url ?? "",
+            body: null,
+            redirected: false,
+            bodyUsed: true,
+            clone: () => {
+              throw new Error("Not implemented");
+            },
+            blob: () => {
+              throw new Error("Not implemented");
+            },
+            formData: () => {
+              throw new Error("Not implemented");
+            },
+            type: "default",
+          };
+          resolve(out);
+        });
+      });
+
+      req.on("error", (err) => {
+        reject(err);
+      });
+
+      if (options.body) {
+        req.write(options.body);
+      }
+      req.end();
+    });
+  }
+}
---
+++ src/wasmer_cli.ts
@@ -0,0 +1,98 @@
+import * as path from "node:path";
+import os from "node:os";
+import fs from "node:fs";
+import * as toml from "jsr:@std/toml";
+
+import { Path } from "./fs.ts";
+
+// The global wasmer config file.
+export interface WasmerConfig {
+  registry?: {
+    active_registry?: string;
+    tokens?: [{ registry: string; token: string }];
+  };
+}
+
+// Load the wasmer CLI config file.
+export function loadWasmerConfig(): WasmerConfig {
+  const p = path.join(os.homedir(), ".wasmer/wasmer.toml");
+  const contents = fs.readFileSync(p, "utf-8");
+  const data = toml.parse(contents);
+  return data;
+}
+
+
+// Parsed output from the "wasmer deploy" command.
+export interface DeployOutput {
+  name: string;
+  appId: string;
+  appVersionId: string;
+  url: string;
+
+  path: Path;
+}
+
+export function parseDeployOutput(stdout: string, dir: Path): DeployOutput {
+  let infoRaw: any;
+  try {
+    infoRaw = JSON.parse(stdout);
+  } catch (err) {
+    throw new Error(
+      `Invalid output data: could not parse output as JSON: '${err}': '${stdout}'`,
+    );
+  }
+
+  let jsonConfig: any;
+  try {
+    jsonConfig = JSON.parse(infoRaw?.json_config);
+  } catch (err) {
+    throw new Error(
+      `Invalid output data: could not parse JSON config: '${err}': '${infoRaw?.jsonConfig}'`,
+    );
+  }
+
+  const fullName = jsonConfig?.meta?.name;
+  if (typeof fullName !== "string") {
+    throw new Error(
+      `Invalid output data: could not extract name from JSON config: '${infoRaw?.jsonConfig}'`,
+    );
+  }
+  const [_owner, name] = fullName.split("/");
+
+  if (typeof infoRaw !== "object") {
+    throw new Error(
+      `Invalid output data: expected JSON object, got '${stdout}'`,
+    );
+  }
+
+  const versionId = infoRaw?.id;
+  if (typeof versionId !== "string") {
+    throw new Error(
+      `Invalid output data: could not extract ID from '${stdout}'`,
+    );
+  }
+
+  const appId = infoRaw?.app?.id;
+  if (typeof appId !== "string") {
+    throw new Error(
+      `Invalid output data: could not extract app ID from '${stdout}'`,
+    );
+  }
+
+  const url = infoRaw?.url;
+  if (typeof url !== "string" || !url.startsWith("http")) {
+    throw new Error(
+      `Invalid output data: could not extract URL from '${stdout}'`,
+    );
+  }
+
+  const info: DeployOutput = {
+    name,
+    appId,
+    appVersionId: versionId,
+    url,
+    path: dir,
+  };
+
+  return info;
+}
