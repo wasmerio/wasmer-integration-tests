171c7d6ef4db515a9e33bd82f01cccf2966f50a0
Ayush Jha <ayush@wasmer.io>
2025-03-05 21:42:44 +0545

    style: pass all lints

--- autobuild.test.ts
+++ autobuild.test.ts
@@ -6,7 +6,7 @@ import { TestEnv } from "./src/env.ts";
  */
 Deno.test("autobuild-wordpress", {}, async (t) => {
   const env = TestEnv.fromEnv();
-  let app_url: string | null = null;
+  let app_url: string;
   await t.step("deploy via autobuild", async () => {
     const extra_data = {
       wordpress: {
@@ -17,17 +17,17 @@ Deno.test("autobuild-wordpress", {}, async (t) => {
         siteName: "My Wordpress Site",
       },
     };
-    url = await env.deployAppFromRepo(
+    app_url = await env.deployAppFromRepo(
       "https://github.com/wasmerio/wordpress",
       extra_data,
     );
   });
   await t.step("check app is live", async () => {
-    if (!url) {
+    if (!app_url) {
       fail("Failed to deploy wordpress app via autobuild");
     }
     // check if the url is live
-    const response = await env.httpClient.fetch(url, { method: "GET" });
+    const response = await env.httpClient.fetch(app_url, { method: "GET" });
     if (response.status !== 200) {
       fail(
         `Failed to fetch deployed wordpress app via autobuild: ${await response
@@ -37,10 +37,9 @@ Deno.test("autobuild-wordpress", {}, async (t) => {
   });
 });
 
-
 Deno.test("autobuild-wordpress: custom branch", {}, async (t) => {
   const env = TestEnv.fromEnv();
-  let app_url: string | null = null;
+  let app_url: string;
   await t.step("deploy via autobuild", async () => {
     const extra_data = {
       wordpress: {
@@ -58,11 +57,44 @@ Deno.test("autobuild-wordpress: custom branch", {}, async (t) => {
     );
   });
   await t.step("check app is live", async () => {
-    if (!url) {
+    if (!app_url) {
       fail("Failed to deploy wordpress app via autobuild");
     }
     // check if the url is live
-    const response = await env.httpClient.fetch(url, { method: "GET" });
+    const response = await env.httpClient.fetch(app_url, { method: "GET" });
+    if (response.status !== 200) {
+      fail(
+        `Failed to fetch deployed wordpress app via autobuild: ${await response
+          .text()}`,
+      );
+    }
+  });
+});
+
+Deno.test("autobuild-wordpress: spanish", {}, async (t) => {
+  const env = TestEnv.fromEnv();
+  let app_url: string;
+  await t.step("deploy via autobuild", async () => {
+    const extra_data = {
+      wordpress: {
+        adminEmail: "something@something.com",
+        adminUsername: "admin",
+        adminPassword: "password123!",
+        language: "es_ES",
+        siteName: "Mi sitio de Wordpress",
+      },
+    };
+    url = await env.deployAppFromRepo(
+      "https://github.com/wasmerio/wordpress",
+      extra_data,
+    );
+  });
+  await t.step("check app is live", async () => {
+    if (!app_url) {
+      fail("Failed to deploy wordpress app via autobuild");
+    }
+    // check if the url is live
+    const response = await env.httpClient.fetch(app_url, { method: "GET" });
     if (response.status !== 200) {
       fail(
         `Failed to fetch deployed wordpress app via autobuild: ${await response
