504f0aa03c1e0d364c9ed1f19e7bc7716bcd0208
Lorentz Kinde <lorentz@wasmer.io>
2025-05-16 21:27:52 +0300

    chore: Cleaned up deployment test slightly

--- tests/deploy/python.test.ts
+++ tests/deploy/python.test.ts
@@ -1,6 +1,5 @@
 import { buildPythonApp, TestEnv } from "../../src/index";
 import * as fs from "node:fs";
-import { assertStringIncludes } from "../../src/testing_tools";
 
 // Test that we can deploy a simple python app
 test.concurrent("deploy python app", async () => {
@@ -12,8 +11,10 @@ test.concurrent("deploy python app", async () => {
   const appInfo = await env.deployApp(app);
 
   const uniquePing = Math.random();
-  const res = await env.fetchApp(appInfo, `${uniquePing}`);
+  const want = `${uniquePing}`;
+  const res = await env.fetchApp(appInfo, want);
   const gotJson = (await res.json()) as { echo: string };
-  expect(gotJson.echo).toBe(uniquePing)
+  const got = gotJson.echo;
+  expect(got).toBe(want);
   await env.deleteApp(appInfo);
 });
